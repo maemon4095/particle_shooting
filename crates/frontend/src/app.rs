c5d995f4e5455bd10a887e0b202f02ee50c797a5
maemon4095 <maemondecocted@gmail.com>
2023-03-06 15:03:30 +0900

    remove dist

--- crates/frontend/src/app.rs
+++
@@ -1,8 +0,0 @@
-use yew::prelude::*;
-
-#[function_component]
-pub fn App() -> Html {
-    html! {
-        <crate::drawing::Drawing/>
-    }
-}
--- crates/frontend/src/main.rs
+++
@@ -1,7 +0,0 @@
-mod app;
-mod drawing;
-mod particle_system;
-mod particles;
-fn main() {
-    yew::Renderer::<app::App>::new().render();
-}
